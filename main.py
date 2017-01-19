c3527d53a41090396ab331b8500cc891d240aeef
Raev_e <raevegnforspam@rambler.ru>
2017-01-19 10:06:26 +0300

    add sleeps

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from const import *
 from proto import *
-
+import random
 
 def action(hour, minute):
     flag = False
     if hour in battle_hours:
         if minute == 5:
+            time.sleep(random.randint(0, 20))
             ask_report()
             log_event('Ask report')
             flag = True
 
     if hour in [h - 1 for h in battle_hours]:
         if minute == 45:
+            time.sleep(random.randint(0, 20))
             castle = get_castle()
             attack(castle)
             log_event('Attack {0}'.format(castle))
